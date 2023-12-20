59f942702cac1d8f4b4a3bb63b8e1e3d9c11f94a
Julian TRANI <73946832+JulianT31@users.noreply.github.com>
2023-12-20 15:33:01 +0100

    add to_json, from_json, __eq__ fucntions

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import uuid
 from time import perf_counter
@@ -6,13 +7,13 @@ import numpy as np
 
 
 class Task:
-    def __init__(self, size):
-        self.identifier = uuid.uuid4()
+    def __init__(self, size=6, identifier=None, a=None, b=None, x=None, time=None):
+        self.identifier = uuid.uuid4() if identifier is None else identifier
         self.size = size
-        self.a = np.random.rand(self.size, self.size)
-        self.b = np.random.rand(self.size)
-        self.x = None
-        self.time = None
+        self.a = np.random.rand(self.size, self.size) if a is None else a
+        self.b = np.random.rand(self.size) if b is None else b
+        self.x = None if x is None else x
+        self.time = None if time is None else time
 
     def work(self):
         start = perf_counter()
@@ -23,8 +24,44 @@ class Task:
         logging.debug("Task ended in " + str(self.time))
         return self.x
 
+    def to_json(self):
+        # Warning attributs values need to be in string
+        dictionnary = {
+            "identifier": self.identifier.__str__(),
+            "size": self.size,
+            "a": self.a.tolist(),
+            "b": self.b.tolist(),
+            "x": self.x,
+            "time": self.time,
+        }
+        json_string = json.dumps(dictionnary)
+        return json_string
+
+    @staticmethod
+    def from_json(txt):
+        dict_data = json.loads(txt)
+        new_task = Task(
+            dict_data["size"],
+            identifier=dict_data["identifier"],
+            a=np.array(dict_data["a"]),
+            b=np.array(dict_data["b"]),
+            x=dict_data["x"],
+            time=dict_data["time"],
+        )
+        return new_task
+
+    def __eq__(self, other):
+        if not isinstance(other, Task):
+            return False
+
+        return (
+            self.size == other.size
+            and np.equal(self.a, other.a).all()
+            and np.equal(self.b, other.b).all()
+            and np.equal(self.x, other.x)
+            and self.time == other.time
+        )
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-
-    Task(6000).work()
